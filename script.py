39a9cf297ab6d40f6b9ecfa41865e91740bc9482
Robbert Struyven <robbertstruyven@gmail.com>
2021-11-23 13:18:32 +0000

    new files

--- script.py
+++ script.py
@@ -6,7 +6,8 @@ labels = [ f.split('num')[1][0] for f in list_files ]
 df= pd.DataFrame(data=[list_files, labels],
                  index=['file', 'target']).T
 
-df.to_csv('mnist_10k_test_images.csv')
+df.to_csv('mnist_10k_test_images.csv', 
+          index = False)
 print(df.head())
 print(df.shape)
 print(df.target.value_counts())
