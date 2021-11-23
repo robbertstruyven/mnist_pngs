9b9ebc26e1febe34da90ddb556eeb3f4b9336015
Robbert Struyven <robbertstruyven@gmail.com>
2021-11-23 13:07:58 +0000

    adding image files and

---
+++ script.py
@@ -0,0 +1,11 @@
+import glob
+import pandas as pd
+
+list_files = glob.glob("test_images/*png")
+labels = [ f.split('num')[1][0] for f in list_files ]
+df= pd.DataFrame(data=[list_files, labels],
+                 index=['file', 'target']).T
+
+print(df.head())
+print(df.shape)
+print(df.target.value_counts())
