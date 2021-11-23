24b07631461a644427b83019955b70443c2f7492
Robbert Struyven <robbertstruyven@gmail.com>
2021-11-23 13:08:43 +0000

    adding csv

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ labels = [ f.split('num')[1][0] for f in list_files ]
 df= pd.DataFrame(data=[list_files, labels],
                  index=['file', 'target']).T
 
+df.to_csv('mnist_labels.csv')
 print(df.head())
 print(df.shape)
 print(df.target.value_counts())
