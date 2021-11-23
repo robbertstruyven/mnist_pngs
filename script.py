71becc340bbfffcbf4ce7b04bdd7037e386d184e
Robbert Struyven <robbertstruyven@gmail.com>
2021-11-23 13:16:14 +0000

    new files

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 import glob
 import pandas as pd
 
-list_files = glob.glob("test_images/*png")
+list_files = glob.glob("mnist_10k_test_images/*png")
 labels = [ f.split('num')[1][0] for f in list_files ]
 df= pd.DataFrame(data=[list_files, labels],
                  index=['file', 'target']).T
 
-df.to_csv('mnist_labels.csv')
+df.to_csv('mnist_10k_test_images.csv')
 print(df.head())
 print(df.shape)
 print(df.target.value_counts())
